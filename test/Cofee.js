4c2b4453ec9b32f8cdf7df2ee7fef3ee13332acc
Arman <armmamyan@gmail.com>
2022-11-10 12:56:11 +0400

    Buy me a coffee contract for sc-creator

---
+++ test/Cofee.js
@@ -0,0 +1,60 @@
+const hre = require("hardhat");
+
+// Logs the tips stored on-chain from coffee purchases.
+async function printTips(tips) {
+  for (const tip of tips) {
+    const timestamp = tip.timestamp;
+    const tipper = tip.name;
+    const tipperAddress = tip.from;
+    const message = tip.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  // Get the example accounts we'll be working with.
+  const [owner, tipper, tipper2] = await hre.ethers.getSigners();
+
+  // We get the contract to deploy.
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+
+  // Deploy the contract.
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
+
+  // Check balances before the coffee purchase.
+  const addresses = [owner.address, tipper.address, buyMeACoffee.address];
+  console.log("== start ==");
+  await printBalances(addresses);
+
+  // Buy the owner a few coffees.
+  const tip = {value: hre.ethers.utils.parseEther("1")};
+  await buyMeACoffee.connect(tipper).buyCoffee("John", "Good Job!", tip);
+  await buyMeACoffee.connect(tipper2).buyCoffee("Doe", "Thanks a lot", tip);
+
+  // Check balances after the coffee purchase.
+  console.log("== bought coffee ==");
+  await printBalances(addresses);
+
+  // Withdraw.
+  await buyMeACoffee.connect(owner).withdrawTips();
+
+  // Check balances after withdrawal.
+  console.log("== withdrawTips ==");
+  await printBalances(addresses);
+
+  // Check out the memos.
+  console.log("== Tip List ==");
+  const tips = await buyMeACoffee.getTips();
+  printTips(tips);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
